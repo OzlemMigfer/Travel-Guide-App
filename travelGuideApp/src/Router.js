705090a3e43a63a22f79a9854a3d0cd47e5d2a62
Your Name <ozlemhayalkurun@gmail.com>
2022-12-05 13:40:46 +0300

    create a project
    first commit

---
+++ travelGuideApp/src/Router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View} from 'react-native';
+
+function App(){
+  return(
+    <View>
+      <Text>Gezi Rehber Uygulaması</Text>
+    </View>
+  );
+};
+
+export default App;

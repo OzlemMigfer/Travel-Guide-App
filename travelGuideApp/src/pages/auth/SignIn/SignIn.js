a535ec4bf4a3b35ec026be3471ea52343cc66eca
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2022-12-07 12:48:33 +0300

    upgrade

--- travelGuideApp/src/pages/auth/SignIn/SignIn.js
+++ travelGuideApp/src/pages/auth/SignIn/SignIn.js
@@ -1,13 +1,1 @@
-import React from "react";
-import {Text, View} from 'react-native';
-import styles from './SignIn.styles';
 
-const SignIn = () => {
-    return(
-        <View style={styles.container}>
-            <Text style={styles.header}>Kayıt Ekranı</Text>
-        </View>
-    );
-};
-
-export default SignIn;
